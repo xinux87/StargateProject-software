3831886d015dcfa8242db78eaf4e0986552498af
xinux87 <xinux87@gmail.com>
2026-05-05 22:43:58 +0200

    updated

--- install/create_venv.sh
+++ install/create_venv.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
-# This script creates a Python virtual environment and installs the required packages
-
-# Set the virtual environment name
+# Detect the real user even when invoked via sudo
+REAL_USER=${SUDO_USER:-$(whoami)}
+USER_HOME=$(getent passwd "$REAL_USER" | cut -d: -f6)
 VENV_NAME="venv_v4"
-DIR="/home/xinux"
+VENV_DIR="$USER_HOME/$VENV_NAME"
+SG1_DIR="$USER_HOME/sg1_v4"
 
-# Create the virtual environment
-cd $DIR
+cd "$USER_HOME"
 
 # Remove the env if it already exists
-[ ! -d './$VENV_NAME' ] && rm -Rf "$DIR/$VENV_NAME"
+[ -d "$VENV_DIR" ] && rm -Rf "$VENV_DIR"
 
-echo "Initializing Python virtual environment in ${DIR}/${VENV_NAME}"
-python3 -m venv "$VENV_NAME"
+echo "Initializing Python virtual environment in $VENV_DIR"
+python3 -m venv "$VENV_DIR"
 
-# Activate the venv and install some dependencies
 echo 'Installing pip setuptools into the virtual environment'
-source "$VENV_NAME/bin/activate"
+source "$VENV_DIR/bin/activate"
 export CFLAGS=-fcommon
 pip install setuptools | sed 's/^/     /'
 
-# Install requirements.txt pip packages
 echo 'Installing requirements.txt dependencies into the Virtual Environment'
-source "$VENV_NAME/bin/activate"
-pip install -r sg1_v4/requirements.txt | sed 's/^/     /'
+pip install -r "$SG1_DIR/requirements.txt" | sed 's/^/     /'
 
 echo 'Deactivating the virtual environment'
 deactivate
